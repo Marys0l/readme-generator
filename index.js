51c5ab99314d29720bd3bab4e8484d68cb48e194
edna galindo <ednagalindo@ednas-MacBook-Pro.local>
2021-08-09 00:20:17 -0400

    fleshed out functions, set up questions, connected js files

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
-const inquirer = require('inquirer')
-const fs = require ('fs')
-const generateMarkdown = require('./utils/ generateMarkdown')
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const generateMarkdown = require('./utils/ generateMarkdown.js')
 
+    const questions = [
+        {
+           type: 'input',
+           message: 'What is the title of your project?',
+           name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'What is the description of your project?',
+            name: 'description'
+        },
+        {
+            type: 'prompt',
+            message: 'What are the installation instructions for your project?',
+            name: 'installation'
+        },
+        {
+           type: 'prompt',
+           message: 'What is the Usage for your project?',
+           name: 'usage' 
+        },
+        {
+            type: 'prompt',
+            message: 'What are the contribution guidleines for your project?',
+            name: 'contribution'
+        },
+        {
+            type: 'prompt',
+            message: 'How can your project be tested',
+            name: 'testing'
+        },
+        {
+            type: 'list',
+            message: 'What license did you use for your project? ',
+            choices: ['MIT','GPL v3','Apache'],
+            name: 'license'
+        },
+        {
+            type: 'prompt',
+            message: 'What is your GitHub username?',
+            name: 'username'
+        },
+        {
+            type: 'prompt',
+            message: 'What is your email address so users can reach you?',
+            name: 'email'
+        },
+    ];
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.error(err);
+        };
+      
+        console.log("READme Created!");
+      });
+
+}
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function (data) {
+        writeToFile('README.md', generateMarkdown(data));
+        console.log(data);
+    }).catch(function (err) {
+        console.error(err)
+    }) 
+}
+init()
--- utils/ generateMarkdown.js
+++ utils/ generateMarkdown.js
@@ -3,36 +3,44 @@ function renderLicenseBadge(license) {
         case 'MIT':
         response = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)(https://opensource.org/licenses/Apache-2.0)'
         break;
-    } return response
-    
-
-}
-
-function renderLicenseLink(license) {
-    switch(license) {
-        case 'GNU GPL v3':
+        case 'GPL v3':
         response = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
         break;
-    } return response 
-
-}
-
-function renderLicenseLink(license) {
-    switch(license) {
         case 'Apache':
         response = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
         break;
     } return response 
-
 }
 
-    
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+    return `#${data.title}
+    ${renderLicenseBadge(data.license)}
+  
+    
+#### Table of Contents
+1. [Project Description](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contribution ](#contributor-info)
+5. [Test Instructions](#test-instructions)
+6. [License](#license)
+7. [Questions](#questions)
+## Description
+* ${data.description}
+## Instructions
+* ${data.instructions}
+## Usage 
+* ${data.usage}
+## Contributing
+* ${data.contributions}
+## Tests
+* ${data.test}
+## License
+* licensed under the ${data.license}
+## Questions
+* Please feel free to send me an email for addtional information
+${data.email}
+* Follow me on Github at [${data.github}](http://github.com/${data.github})`; 
 }
 
 module.exports = generateMarkdown;
